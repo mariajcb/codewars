7fc8b8cbe9a7d86a2f4794bee1603825188e35e0
mariajcb <bogomazm@gmail.com>
2017-10-26 19:34:13 -0600

    solve needle in the haystack

---
+++ 8kyu-needle-in-haystack.js
@@ -0,0 +1,23 @@
+// Can you find the needle in the haystack?
+//
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//
+// After your function finds the needle it should return a message (as a string) that says:
+//
+// "found the needle at position " plus the index it found the needle
+
+function findNeedle(haystack) {
+  for (var i = 0; i < haystack.length; i++) {
+    if (haystack[i] === "needle") {
+      return "found the needle at position " + haystack.indexOf(haystack[i]);
+    }
+  }
+}
+
+//wait, there is an easier way to do this
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+
+//ES6
+const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`;
