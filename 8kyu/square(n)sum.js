24ddf6b2401917755d8fbb6c007468232dcb1b8c
mariajcb <bogomazm@gmail.com>
2019-01-31 18:02:10 -0700

    solve 8 kyu square(n) sum

---
+++ 8kyu/square(n)sum.js
@@ -0,0 +1,18 @@
+// Complete the square sum method so that it squares each number passed into it and then sums the results together.
+//
+// For example: squareSum([1, 2, 2]) should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += (numbers[i] * numbers [i]);
+  }
+  return sum;
+}
+
+//top codewars solution:
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
